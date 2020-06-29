9246e9de8b0684ff46324a1386b407827a398537
saranraj.shri <shrisaranraj2001@gmail.com>
2020-06-29 01:17:54 -0400

    Updates

---
+++ precache-manifest.355be98f2e3781ef9e3bb6f05b20b3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff5a4236bb148ec9f9340bb00bec2bd",
+    "url": "/Algorithm-Visualizer/index.html"
+  },
+  {
+    "revision": "4ddb19902c83ef70f27a",
+    "url": "/Algorithm-Visualizer/static/css/main.705f1747.chunk.css"
+  },
+  {
+    "revision": "781b887caf82b709b36c",
+    "url": "/Algorithm-Visualizer/static/js/2.9a6cd33e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Algorithm-Visualizer/static/js/2.9a6cd33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddb19902c83ef70f27a",
+    "url": "/Algorithm-Visualizer/static/js/main.6e79e3b8.chunk.js"
+  },
+  {
+    "revision": "3f04e77c0d24dbe827fe",
+    "url": "/Algorithm-Visualizer/static/js/runtime-main.a3779ad4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Algorithm-Visualizer/precache-manifest.5ef19b4d78bd9b8b5a24d6b58b7d3f78.js"
+  "/Algorithm-Visualizer/precache-manifest.355be98f2e3781ef9e3bb6f05b20b3fc.js"
 );
 
 self.addEventListener('message', (event) => {
